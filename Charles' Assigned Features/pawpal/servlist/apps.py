c02f348ab82aac0b39143e9e160a0cdae1982775
Charuzuuuu <charuzudarwin@gmail.com>
2024-09-23 04:33:04 +0800

    first commit

---
+++ Charles' Assigned Features/pawpal/servlist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ServlistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'servlist'
---
+++ Charles' Assigned Features/pawpal/servlist/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.service_listings, name='service_listings'),
+]
