3633b1e3c3ad77ef27b7a24f42b1dcbecb57f2b8
minagawaYuki <markadrian.baring@cit.edu>
2024-09-22 21:15:48 +0800

    Django Project Initialization

--- PawPal/PawPal/wsgi.py
+++ PawPal/PawPal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Pawpal project.
+WSGI config for PawPal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pawpal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PawPal.settings')
 
 application = get_wsgi_application()
