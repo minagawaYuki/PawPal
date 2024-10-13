5214c4d2247d6916178e1de87a3169adf7ce11a6
Charuzuuuu <charuzudarwin@gmail.com>
2024-10-14 01:23:55 +0800

    displayed book sched

--- Charles' Assigned Features/pawpal/servlist/admin.py
+++ Charles' Assigned Features/pawpal/servlist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pet, Service, Booking
 
 # Register your models here.
+admin.site.register(Pet)
+admin.site.register(Service)
+admin.site.register(Booking)
