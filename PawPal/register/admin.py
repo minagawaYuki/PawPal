184cfa5a4798a01798d003a19cb4fd3cfdcdb128
Charuzuuuu <charuzudarwin@gmail.com>
2024-10-14 20:04:27 +0800

    log in and book sched

--- PawPal/register/admin.py
+++ PawPal/register/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Pet, Service, Booking
+#from .models import Pet, Service, Booking
 
 # Register your models here.
-admin.site.register(Pet)
-admin.site.register(Service)
-admin.site.register(Booking)
+#admin.site.register(Pet)
+#admin.site.register(Service)
+#admin.site.register(Booking)
