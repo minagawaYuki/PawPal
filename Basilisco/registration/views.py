b1ff5ee49dc739cc8a4044d66e072a845b9dc33c
Rienel <rieub123@gmail.com>
2024-10-15 11:53:51 +0800

    full

--- Basilisco/registration/views.py
+++ Basilisco/registration/views.py
@@ -35,8 +35,9 @@ def login_view(request):
             login(request, user)
             return redirect('home')
         else:
+            error = 'Invalid username or password'
             messages.error(request, 'Invalid username or password')
-            return render(request, 'login.html')
+            return render(request, 'login.html', {'error': error})
     else:
         return render(request, 'login.html')
 
