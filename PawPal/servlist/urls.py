17a34080b009ba481af4a9ac0a97a5f93ec897bf
GrandKou <rieub123@gmail.com>
2024-11-03 23:23:26 +0800

    partial notification

--- PawPal/servlist/urls.py
+++ PawPal/servlist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from .views import book_schedule
+from .views import book_schedule, mark_notifications_as_read
 
 urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'), 
     path('book/', views.book_schedule, name='book'),
+    path('mark_notifications_as_read/', mark_notifications_as_read, name='mark_notifications_as_read'),
+    path('update-booking-status/<int:booking_id>/<str:new_status>/', views.update_booking_status, name='update_booking_status'),
 ]
